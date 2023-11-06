e6e9e67da3172d0c55e8a3236cda7ddd41ac3707
syonus890 <syonus@andrew.cmu.edu>
2023-11-05 20:36:49 -0500

    Final project start

---
+++ WeatherWebsite/Weather.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const submitButton = document.getElementById("submit");
+    submitButton.addEventListener("click", getWeatherAndOutfit);
+});
+
+function getWeatherAndOutfit() {
+    const locationInput = document.getElementById("location");
+    const location = locationInput.value;
+
+    // Call a weather API to get weather data for the location
+    // Replace 'API_KEY' and 'API_URL' with your API details
+    fetch(`API_URL?location=${location}&apiKey=API_KEY`)
+        .then(response => response.json())
+        .then(data => {
+            const temperature = data.main.temp;
+            const conditions = data.weather[0].main;
+            
+            // Generate an outfit suggestion based on temperature and conditions
+            const outfitSuggestion = generateOutfit(temperature, conditions);
+            
+            const weatherForecast = document.getElementById("weather-forecast");
+            weatherForecast.innerHTML = `For ${location}, it's ${temperature}°C and ${conditions}. You should wear: ${outfitSuggestion}`;
+        })
+        .catch(error => {
+            console.error("Error fetching weather data:", error);
+        });
+}
+
+function generateOutfit(temperature, conditions) {
+    // Add your logic to generate outfit suggestions based on temperature and conditions here.
+    // For example, you can use if statements to determine the outfit.
+    // You may need to define different outfits for various weather conditions.
+    
+    // Example logic:
+    if (temperature < 10) {
+        return "Wear a heavy coat, gloves, and a scarf.";
+    } else if (temperature < 20) {
+        return "Wear a jacket and jeans.";
+    } else {
+        return "It's warm outside, wear light clothing.";
+    }
+}
